3158e3bc71550b25d45e976fbe3dad8935381931
modestas <mbujanauskas@gmail.com>
2018-10-06 19:51:27 +0300

    Initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,55 @@
+//'use strict';
+
+let registrationNav = Array.from(document.querySelectorAll(".terms nav div"));
+
+registrationNav.forEach((element, index) => {
+    element.addEventListener('click', (e) => {
+        registrationNav.forEach((ele) => {
+            ele.classList.remove('active');
+        })
+        e.currentTarget.classList.add("active");
+        changeIcon();
+        showFix(index)
+    })
+})
+
+function showFix(index){
+    let terms = Array.from(document.querySelectorAll(".terms > .collapse"));
+    if(terms[index].classList.contains('show')){
+        terms[index].classList.remove('show')
+    }  
+}
+
+
+
+function changeIcon(){
+    let iconEleActive = document.querySelector(".terms-container.active a > i");
+    let iconEleInactive = document.querySelectorAll(".terms-container a > i");
+
+    iconEleInactive.forEach((icon) =>{
+        icon.classList.remove("icon-minus")
+        icon.classList.add("icon-plus")
+    })
+    iconEleActive.classList.remove("icon-plus");
+    iconEleActive.classList.add("icon-minus")   
+}
+
+// BOOTSTRAP 4 form validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      let forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      let validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
